7a680d0176a91d1c0a7093871d34ad93ad79a26e
alsanger <san-mail@ukr.net>
2025-05-15 13:59:33 +0300

    Configure basic routes for pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'dashboard'])->name('dashboard');
+Route::get('/profile', [PageController::class, 'profile'])->name('profile');
+Route::get('/settings', [PageController::class, 'settings'])->name('settings');
+
+Route::resource('tasks', TaskController::class);
